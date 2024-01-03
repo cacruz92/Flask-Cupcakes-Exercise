5b1e039fa97578e648ee8e59b79b4da4046ca2d7
Christian Cruz <christian.cruz92@hotmail.com>
2024-01-02 22:02:51 -0500

    fixed the code to allow the seed file to be run

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, jsonify, render_template
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+connect_db(app)
+
+with app.app_context():
+    db.create_all()
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
--- seed.py
+++ seed.py
@@ -2,21 +2,23 @@ from app import app
 from models import db, Cupcake
 
 
-db.drop_all()
-db.create_all()
+with app.app_context():
 
-c1 = Cupcake(
-    flavor="cherry",
-    size="large",
-    rating=5,
-)
+    db.drop_all()
+    db.create_all()
 
-c2 = Cupcake(
-    flavor="chocolate",
-    size="small",
-    rating=9,
-    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
-)
+    c1 = Cupcake(
+        flavor="cherry",
+        size="large",
+        rating=5,
+    )
 
-db.session.add_all([c1, c2])
-db.session.commit()
+    c2 = Cupcake(
+        flavor="chocolate",
+        size="small",
+        rating=9,
+        image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
+    )
+
+    db.session.add_all([c1, c2])
+    db.session.commit()
